00b5b6f6533787a91168d1536e24095616cb4f15
Finalmatt <60184241+Finalmatt@users.noreply.github.com>
2020-05-11 15:56:12 +0200

    Add files via upload

---
+++ FirstChatBot/utils.py
@@ -0,0 +1,95 @@
+import nltk
+import pickle
+import re
+import numpy as np
+import sklearn 
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+# Paths for all resources for the bot.
+RESOURCE_PATH = {
+    'INTENT_RECOGNIZER': 'intent_recognizer.pkl',
+    'TAG_CLASSIFIER': 'tag_classifier.pkl',
+    'TFIDF_VECTORIZER': 'tfidf_vectorizer.pkl',
+    'THREAD_EMBEDDINGS_FOLDER': 'thread_embeddings_by_tags',
+    'WORD_EMBEDDINGS': 'StarspaceEmbeddingTrained.tsv',
+}
+
+
+def text_prepare(text):
+    """Performs tokenization and simple preprocessing."""
+
+    replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
+    bad_symbols_re = re.compile('[^0-9a-z #+_]')
+    stopwords_set = set(stopwords.words('english'))
+
+    text = text.lower()
+    text = replace_by_space_re.sub(' ', text)
+    text = bad_symbols_re.sub('', text)
+    text = ' '.join([x for x in text.split() if x and x not in stopwords_set])
+
+    return text.strip()
+
+
+def load_embeddings(embeddings_path):
+    """Loads pre-trained word embeddings from tsv file.
+
+    Args:
+      embeddings_path - path to the embeddings file.
+
+    Returns:
+      embeddings - dict mapping words to vectors;
+      embeddings_dim - dimension of the vectors.
+    """
+
+    # Hint: you have already implemented a similar routine in the 3rd assignment.
+    # Note that here you also need to know the dimension of the loaded embeddings.
+    # When you load the embeddings, use numpy.float32 type as dtype
+
+    with open(embeddings_path) as e:
+      embeddings = {}
+      for line in e.readlines():
+        row = line.strip().split('\t')
+        embeddings[row[0]] = np.array(row[1:], dtype=np.float32)
+    embeddings_dim = embeddings[list(embeddings)[0]].shape[0]
+
+    return embeddings, embeddings_dim
+
+
+
+
+
+def question_to_vec(question, embeddings, dim):
+    """
+        question: a string
+        embeddings: dict where the key is a word and a value is its' embedding
+        dim: size of the representation
+
+        result: vector representation for the question
+    """
+    question_split = question.split()
+
+    words_matrice = np.zeros((dim,), dtype=np.float32)
+    count = 0
+    for word in question_split:
+      if word in embeddings:
+       #words_matrice += embeddings[word]
+       np.add(words_matrice, embeddings[word], out=words_matrice, casting="unsafe")
+       count += 1
+       
+
+    
+    
+    if count == 0:
+      return np.zeros(dim)
+    else:
+      sequence_matrice = words_matrice / count
+    return sequence_matrice
+
+
+
+def unpickle_file(filename):
+    """Returns the result of unpickling the file content."""
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
